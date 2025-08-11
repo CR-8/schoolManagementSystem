64e6345c992cfb1d3a484dc739f2677b4fa20997
Pratyush Tiwari <pratyush_be23ds043@srmcem.ac.in>
2025-08-11 13:18:59 +0530

    fixes

--- index.js
+++ index.js
@@ -6,25 +6,28 @@ import schoolRoutes from "./routes/schools.js"
 dotenv.config();
 
 const app = express();
-
 app.use(express.json());
 
 const db = mysql2.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
-    if(err){
-        console.error("Error Connecting to Database : ",err);
+    if (err) {
+        console.error("Database connection failed:", err);
         process.exit(1);
     }
-    else {
+        else {
         console.log("Connection to Database Successfull .")
     }
-})
+    console.log("Connected to MySQL database");
+    console.log("Host:", process.env.DB_HOST);
+    console.log("Database:", process.env.DB_NAME);
+});
 
 app.use("/schools", schoolRoutes);
 
